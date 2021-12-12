d4f6a033881560b1ff02858fb48e85e3d61dcc8d
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2021-12-13 00:01:02 +0300

    first commit

---
+++ BasePython/homework_01/main.py
@@ -0,0 +1,54 @@
+"""
+HW #1
+Functions and data sctructures.
+"""
+
+
+def power_numbers(*args):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    return [n**2 for n in args]
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+FILTER_TYPES = [ODD, EVEN, PRIME]
+
+
+def is_prime(n) -> bool:
+    """
+    Returns if passed number is prime.
+    """
+    if n < 2:
+        return False
+    for div in range(2, int(n ** 0.5) + 1):
+        if n % div == 0:
+            return False
+    return True
+
+
+def filter_numbers(nums, filter_type):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    assert filter_type in FILTER_TYPES, "Allowed filter_type: {FILTER_TYPES}"
+
+    if filter_type == ODD: 
+        return [n for n in nums if n % 2 != 0]
+    elif filter_type == EVEN:
+        return [n for n in nums if n % 2 == 0]
+    else:
+        return [n for n in nums if is_prime(n)]
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# tmp
-tmp stuff
+# Edu Courses Homeworks
+
+The repo for educational courses assignments.
+
+__BasePython__ - tool for SE on python, basic toolkit
