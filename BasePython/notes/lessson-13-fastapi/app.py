5e985ee220b0d086196e5e00ba529e6e20174bbf
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2022-02-28 01:08:45 +0300

    Notes for the lesson about FastAPI

---
+++ BasePython/notes/lessson-13-fastapi/app.py
@@ -0,0 +1,62 @@
+from os import stat
+from fastapi import FastAPI, Body, HTTPException, status, Depends
+
+import crud
+from schemes import UserIn, UserOut, User, CalcInput
+from dependencies import get_user_by_auth_token
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
+
+
+@app.get("/hello")
+def hello(name: str):
+    return {"message": f"Hello {name}."}
+
+
+@app.get("/items/{item_id}", tags=["Items"])
+def get_item_by_id(item_id: int):
+    return {"id": item_id}
+
+
+@app.post("/items", tags=["Items"])
+def create_item(data: dict = Body(...)):
+    return {"data": data} 
+
+
+@app.get("/sub")
+def subtract_values(calc_input: CalcInput = Depends()):
+    return {
+        "data": calc_input.dict(),
+        "result": calc_input.a - calc_input
+    }
+
+
+@app.get("/users", response_model=list[UserOut], tags=["Users"])
+def list_users():
+    return crud.list_users()
+
+
+@app.post("/users/me", response_model=UserOut, tags=["Users"])
+def get_auth_user(user: User = Depends(get_user_by_auth_token)):
+    return user
+
+
+@app.post("/users", response_model=UserOut, tags=["Users"])
+def create_user(user_in: UserIn):
+    user = crud.create_user(user_in)
+    return user
+
+
+@app.get("/users/{user_id}", response_model=UserOut, tags=["Users"])
+def get_user_by_id(user_id: int):
+    user = crud.get_user_by_id(user_id)
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"User {user_id} not found.")
